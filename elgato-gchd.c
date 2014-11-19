b7e8ea05c6974a46c46e9f7e34576b29bc2f1a32
Tolga Cakir <tolga@cevel.net>
2014-11-19 03:44:22 +0100

    major clean up; loading firmware via bulk transfer works now

--- elgato-gchd.c
+++ elgato-gchd.c
@@ -1,124 +1,171 @@
 #include <stdio.h>
-#include <libusb-1.0/libusb.h>
+#include <stdlib.h>
 #include <unistd.h>
 
-#define ELGATO_VID		0x0fd9
-#define GAME_CAPTURE_HD	0x004e
+#include <sys/types.h>
 
-/*
- *  Check for VID & PID
- */
-int is_usbdevblock(libusb_device *dev)
-{
-	struct libusb_device_descriptor desc;
-	libusb_get_device_descriptor(dev, &desc);
+#include <libusb-1.0/libusb.h>
 
-	if (desc.idVendor == ELGATO_VID && desc.idProduct == GAME_CAPTURE_HD) {
+/* constants */
+#define ELGATO_VENDOR		0x0fd9
+#define GAME_CAPTURE_HD_PID	0x004e
+
+#define EP_OUT			0x02
+#define INTERFACE_NUM	0x00
+#define CONFIGURATION	0x01
+
+#define CHUNK_SIZE		0x4000
+
+/* global structs */
+static struct libusb_device_handle *devh = NULL;
+
+int init_dev_handler() {
+	if (libusb_init(NULL)) {
+		fprintf(stderr, "Error initializing libusb.");
+		return 1;
+	}
+
+	//libusb_set_debug(NULL, LIBUSB_LOG_LEVEL_DEBUG);
+
+	devh = libusb_open_device_with_vid_pid(NULL, ELGATO_VENDOR, GAME_CAPTURE_HD_PID);
+	if (!devh) {
+		fprintf(stderr, "Unable to find device.");
 		return 1;
 	}
 
 	return 0;
 }
 
-int main(void) {
-	// discover devices
-	libusb_device **list;
-	libusb_device *found = NULL;
-	libusb_context *ctx = NULL;
-	int attached = 0;
+int get_interface() {
+	if (libusb_kernel_driver_active(devh, INTERFACE_NUM)) {
+		libusb_detach_kernel_driver(devh, INTERFACE_NUM);
+	}
 
-	libusb_init(&ctx);
-	libusb_set_debug(ctx, LIBUSB_LOG_LEVEL_DEBUG); 
-	ssize_t cnt = libusb_get_device_list(ctx, &list);
-	ssize_t i = 0;
-	int err = 0;
-	if (cnt < 0) {
-		printf("no usb devices found\n");
-
+	if (libusb_set_configuration(devh, CONFIGURATION)) {
+		fprintf(stderr, "Could not activate configuration.");
 		return 1;
 	}
 
-	// find our device
-	for (i = 0; i < cnt; i++) {
-		libusb_device *device = list[i];
-		if (is_usbdevblock(device)) {
-			found = device;
+	if (libusb_claim_interface(devh, INTERFACE_NUM)) {
+		fprintf(stderr, "Failed to claim interface.");
+		return 1;
+	}
+
+	return 0;
+}
+
+void clean_up() {
+	if (devh) {
+		libusb_release_interface(devh, INTERFACE_NUM);
+		libusb_close(devh);
+	}
+
+	libusb_exit(NULL);
+}
+
+void read_config(uint16_t wValue, uint16_t wIndex, uint16_t wLength) {
+	unsigned char *recv;
+
+	recv = calloc(wLength, sizeof(unsigned char));
+	libusb_control_transfer(devh, 0xc0, 0xbc, wValue, wIndex, recv, wLength, 0);
+	free(recv);
+}
+
+void write_config(uint16_t wValue, uint16_t wIndex, uint16_t wLength) {
+	unsigned char *send;
+
+	send = calloc(wLength, sizeof(unsigned char));
+	libusb_control_transfer(devh, 0x40, 0xbc, wValue, wIndex, send, wLength, 0);
+	free(send);
+}
+
+void configure_dev() {
+	read_config(0x0800, 0x0094, 4);
+
+	read_config(0x0800, 0x0098, 4);
+
+	read_config(0x0800, 0x0010, 4);
+
+	read_config(0x0800, 0x0014, 4);
+
+	read_config(0x0800, 0x0018, 4);
+
+	write_config(0x0900, 0x0000, 2);
+
+	read_config(0x0900, 0x0014, 2);
+
+	read_config(0x0800, 0x2008, 2);
+
+	read_config(0x0900, 0x0074, 2);
+
+	read_config(0x0900, 0x01b0, 2);
+
+	read_config(0x0800, 0x2008, 2);
+
+	read_config(0x0800, 0x2008, 2);
+
+	/* this is an important step for sending the firmware. TODO: more elegant */
+	unsigned char send[2];
+
+	send[0] = 0x00;
+	send[1] = 0x04;
+
+	libusb_control_transfer(devh, 0x40, 0xbc, 0x0900, 0x0074, send, 2, 0);
+
+	write_config(0x0900, 0x01b0, 2);
+}
+
+void load_firmware(const char *file) {
+	int transfer;
+	unsigned char data[CHUNK_SIZE];
+
+	for (int i = 0; i < CHUNK_SIZE; i++) {
+		data[i] = 0;
+	}
+
+	FILE *bin;
+	bin = fopen(file, "rb");
+
+	while(1) {
+		for (int i = 0; i < CHUNK_SIZE; i++) {
+			data[i] = 0;
+		}
+
+		int ret = fread(data, sizeof(data), 1, bin);
+
+		/* TODO: only transfer amount of read Bytes */
+		libusb_bulk_transfer(devh, 0x02, data, sizeof(data), &transfer, 1000);
+
+		if (!ret) {
 			break;
 		}
+
+		printf("Bulk transfer: %d Bytes\n", transfer);
 	}
 
-	if (found) {
-		printf("found usb-dev-block!\n");
-    		libusb_device_handle *handle;	
-		err = libusb_open(found, &handle);
-		if (err) {
-			printf("Unable to open usb device\n");
-			return 1;
-		}
+	fclose(bin);
+}
 
-		if (libusb_kernel_driver_active(handle,0)) { 
-			printf("Device busy...detaching...\n"); 
-			libusb_detach_kernel_driver(handle,0); 
-			attached = 1;
-		} else printf("Device free from kernel\n"); 
-
-		libusb_set_configuration(handle, 1);
-
-		err = libusb_claim_interface(handle, 0);
-		if (err) {
-			printf("Failed to claim interface.");
-			switch (err) {
-			case LIBUSB_ERROR_NOT_FOUND:	printf("not found\n");	break;
-			case LIBUSB_ERROR_BUSY:		printf("busy\n");		break;
-			case LIBUSB_ERROR_NO_DEVICE:	printf("no device\n");	break;
-			default:			printf("other\n");		break;
-			}
-
-			return 1;
-		}
-
-		unsigned char data[2048];
-		unsigned char recv[256];
-
-		int transferred = 0;
-
-		libusb_control_transfer(handle, 0xc0, 0xbc, 0x0800, 0x0094, recv, 4, 0);
-		libusb_control_transfer(handle, 0xc0, 0xbc, 0x0800, 0x0098, recv, 4, 0);
-		libusb_control_transfer(handle, 0xc0, 0xbc, 0x0800, 0x0010, recv, 4, 0);
-		libusb_control_transfer(handle, 0xc0, 0xbc, 0x0800, 0x0014, recv, 4, 0);
-		libusb_control_transfer(handle, 0xc0, 0xbc, 0x0800, 0x0018, recv, 4, 0);
-
-		libusb_control_transfer(handle, 0x40, 0xbc, 0x0900, 0x0000, recv, 2, 0);
-
-		libusb_control_transfer(handle, 0xc0, 0xbc, 0x0900, 0x0014, recv, 2, 0);
-		libusb_control_transfer(handle, 0xc0, 0xbc, 0x0800, 0x2008, recv, 2, 0);
-		libusb_control_transfer(handle, 0xc0, 0xbc, 0x0900, 0x0074, recv, 2, 0);
-		libusb_control_transfer(handle, 0xc0, 0xbc, 0x0900, 0x01b0, recv, 2, 0);
-		libusb_control_transfer(handle, 0xc0, 0xbc, 0x0800, 0x2008, recv, 2, 0);
-		libusb_control_transfer(handle, 0xc0, 0xbc, 0x0800, 0x2008, recv, 2, 0);
-
-		libusb_control_transfer(handle, 0x40, 0xbc, 0x0900, 0x0074, recv, 2, 0);
-		libusb_control_transfer(handle, 0x40, 0xbc, 0x0900, 0x01b0, recv, 2, 0);
-
-		FILE *idle_bin;
-
-		idle_bin = fopen("firmware/mb86h57_h58_idle.bin", "rb");
-		fread(data, sizeof(data), 1, idle_bin);
-		libusb_bulk_transfer(handle, 0x02, data, sizeof(data), &transferred, 1000);
-		fclose(idle_bin);
-
-		libusb_release_interface(handle, 0);
-
-		//if we detached kernel driver, reattach.
-		if (attached == 1) {
-			libusb_attach_kernel_driver( handle, 0 );
-		}
-
-		libusb_close(handle);
+int main() {
+	/* initialize device handler */
+	if (init_dev_handler()) {
+		goto end;
 	}
 
-	libusb_free_device_list(list, 1);
-	libusb_exit(ctx);
+	/* detach kernel driver and claim interface */
+	if (get_interface()) {
+		goto end;
+	}
+
+	/* configure device */
+	configure_dev();
+
+	/* load "idle" firmware */
+	load_firmware("firmware/mb86h57_h58_idle.bin");
+
+end:
+	/* clean up */
+	clean_up();
 
 	return 0;
 }
