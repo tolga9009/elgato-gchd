df029e12623daabd85883360251deaa81b4ca3a4
Tolga Cakir <tolga@cevel.net>
2015-02-21 17:50:13 +0100

    added commandline arguments to ov2lusb and improved the code

--- utilities/ov2lusb/ov2lusb.cpp
+++ utilities/ov2lusb/ov2lusb.cpp
@@ -1,26 +1,52 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
 #include <regex>
+#include <string>
 
-int main() {
-	int line = 0;
-	std::string str;
+int main(int argc, char **argv) {
+	/* exit, if number of args is incorrect */
+	if(argc != 3) {
+		std::cout << "Usage: " << argv[0] << " <file> <device destination>" << std::endl;
 
-	std::ifstream input("input.txt");
+		return 1;
+	}
 
-	std::string regex_str = "SETUP:.*([[:digit:]].[[:digit:]])";
-	std::regex reg(regex_str);
+	std::string filename(argv[1]);
+	std::string device(argv[2]);
+
+	/* open up the file */
+	std::ifstream textfile(filename);
+
+	if(!textfile) {
+		std::cout << "File not found. Terminating." << std::endl;
+
+		return 1;
+	}
+
+	/* setting up regex */
+	std::string strbuf;
+
+	/* SETUP - match[1]: device destination, match[2]: endpoint */
+	std::string str_rgx_setup = "SETUP:.*([[:digit:]]).([[:digit:]])";
+
+	std::regex rgx_setup(str_rgx_setup);
 	std::smatch match;
 
-	while(!input.eof()) {
+	int line = 0;
+
+	/* searching for matches, line for line using regex */
+	while(!textfile.eof()) {
 		++line;
-		std::getline(input, str);
+		std::getline(textfile, strbuf);
 
-		if(std::regex_search(str, match, reg)) {
-			std::cout << "String found @ line " << line << std::endl;
-
+		if(std::regex_search(strbuf, match, rgx_setup)) {
 			for(unsigned long i = 0; i < match.size(); i++) {
-				std::cout << "Match: " << match[i] << std::endl;
+				if(match[1] != device) {
+					std::cout << "Breaking loop" << std::endl;
+					break;
+				} else {
+					std::cout << "Line " << line << ": Match: " << match[i] << std::endl;
+				}
 			}
 		}
 	}
