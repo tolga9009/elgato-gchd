42db05160a1d506dbdf514694b4c954f1783ed0a
Tolga Cakir <tolga@cevel.net>
2015-06-22 21:29:17 +0200

    added license note

--- elgato-gchd.c
+++ elgato-gchd.c
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2014 - 2015 Tolga Cakir <tolga@cevel.net>
+ *
+ * This source file is part of Elgato Game Capture HD Linux driver and is
+ * distributed under the MIT License. For more information, see LICENSE file.
+ */
+
 #include <fcntl.h>
 #include <signal.h>
 #include <stdio.h>
