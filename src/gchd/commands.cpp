4b2895db3825eb2fa9301018cd61d1a6b2e41b77
Tolga Cakir <tolga@cevel.net>
2016-05-19 22:47:07 +0200

    removed comments from header file
    Signed-off-by: Tolga Cakir <tolga@cevel.net>

--- src/gchd/commands.cpp
+++ src/gchd/commands.cpp
@@ -68,6 +68,23 @@ uint8_t count_bits(uint16_t data) {
 	return i;
 }
 
+/**
+ * Reverse engineered function from official drivers. Used to set device states
+ * and modes. scmd() is the short form for system command.
+ *
+ * @param command there are currently three commands, we have identified. A
+ *  value of 1 stands for IDLE command. A value of 4 means INIT and is used for
+ *  loading firmwares onto the device. A value of 5 means STATE_CHANGE and is
+ *  used to control the device's encoding procedure.
+ * @param mode there is only one use case known so far. It's for setting
+ *  encoding mode.
+ * @param data applies to send[4] and send[5]. The 16-bit integer data needs to
+ *  be split up into two 8-bit integers. It holds data to further specify
+ *  the command parameter. In conjunction with STATE_CHANGE, it is used to set
+ *  specific encoding states: 1 means STOP, 2 means START and 4 means NULL.
+ *  Setting STATE_CHANGE to NULL will make the device output an empty data
+ *  stream during the encoding process.
+ */
 void GCHD::scmd(uint8_t command, uint8_t mode, uint16_t data) {
 	uint8_t send[6] = {0};
 	send[2] = command;
@@ -80,7 +97,22 @@ void GCHD::scmd(uint8_t command, uint8_t mode, uint16_t data) {
 	libusb_control_transfer(devh_, 0x40, 0xb8, 0x0000, 0x0000, send, 6, 0);
 }
 
-// TODO complete overhaul - not working, causing strange artefacts
+/**
+ * Reverse engineered function from official drivers. Used to set specific
+ * device parameters, like H.264 profile, video size and audio bitrate.
+ *
+ * @param wIndex offset, where requests are passed to.
+ * @param shift bit shift left operations to the whole send[8] array. If any
+ *  bit gets cut off, substracting wIndex by 1 will be equal to a bit shift
+ *  right operation of 8. It's used for readibility. Minimum value is 0 and
+ *  maximum is 16.
+ * @param range applies to send[4] and send[5]. Sets n bits to 1, starting from
+ *  the first bit in send[5]. If it's greater than 8, bits are set in send[4].
+ *  Minimum value is 0 and maximum is 16.
+ * @param data applies to send[0] and send[1]. The 16-bit integer data needs to
+ *  be split up into two 8-bit integers. It holds the actual data, like video
+ *  vertical size and bitrate.
+ */
 void GCHD::sparam(uint16_t wIndex, uint8_t shift, uint8_t range, uint16_t data) {
 	uint8_t send[8] = {0};
 
@@ -140,6 +172,15 @@ void GCHD::sparam(uint16_t wIndex, uint8_t shift, uint8_t range, uint16_t data) 
 	libusb_control_transfer(devh_, 0x40, 0xbc, 0x0001, wIndex, send, 8, 0);
 }
 
+/**
+ * Reverse engineered function from official drivers. Used to configure the
+ * device.
+ *
+ * @param wIndex offset, where requests are passed to.
+ * @param data applies to send[0] and send[1]. The 16-bit integer data needs to
+ *  be split up into two 8-bit integers. It holds the actual data, like video
+ *  vertical size and bitrate.
+ */
 void GCHD::slsi(uint16_t wIndex, uint16_t data) {
 	uint8_t send[2] = {0};
 
@@ -150,6 +191,11 @@ void GCHD::slsi(uint16_t wIndex, uint16_t data) {
 	libusb_control_transfer(devh_, 0x40, 0xbc, 0x0000, wIndex, send, 2, 0);
 }
 
+/**
+ * Loads firmware to the device
+ *
+ * @param file relative path to binary firmware file
+ */
 void GCHD::dlfirm(const char *file) {
 	int transfer;
 
